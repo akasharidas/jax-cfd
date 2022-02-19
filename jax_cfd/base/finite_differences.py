28843f5f08bdef0c3df912e3a9dcdf9068fb7214
Akash Haridas <58511267+akasharidas@users.noreply.github.com>
2022-02-19 08:42:37 +0530

    Update finite_differences.py

--- jax_cfd/base/finite_differences.py
+++ jax_cfd/base/finite_differences.py
@@ -126,8 +126,8 @@ def forward_difference(u, axis=None):
 
 def laplacian(u: GridVariable) -> GridArray:
   """Approximates the Laplacian of `u`."""
-  scales = np.square(1 / np.array(u.grid.step, dtype=u.dtype))
-  result = -2 * u.array * np.sum(scales)
+  scales = jnp.square(1 / jnp.array(u.grid.step, dtype=u.dtype))
+  result = -2 * u.array * jnp.sum(scales)
   for axis in range(u.grid.ndim):
     result += stencil_sum(u.shift(-1, axis), u.shift(+1, axis)) * scales[axis]
   return result
